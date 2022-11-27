9ee97e81330dfdde693dfa97bff4585598ef6758
Mohamed Sidow <sidowmohamed35@gmail.com>
2022-11-27 20:21:45 +0000

    updated index.js code

--- README.md
+++ README.md
@@ -3,25 +3,29 @@
   ## Description
   
   ## Table OF Contents
-  * [Installation](##Intro)
-  * [Usage](##Usage)
-  * [Licence](##Licence)
-  * [Contributers](##Contributers)
-  * [Tests](##Tests)
-  * [Questions](##Questions)
+  1. [ Installation. ](#install)
+  2. [ Usage. ](#Usg)
+  3. [ Licence. ](#Lic)
+  4. [ Contributing. ](#Con)
+  5. [ Tests. ](#Ts)
+  6. [ Questions. ](#Que)
   
-
-## Installation
+<a name="install"></a>
+## 1. Installation
 In order for this Program to run,  is required to be installed.
-## Usage
+<a name="#Usg"></a>
+## 2. Usage
 Using this Program requires you to: .
-## Licence
+<a name="Lic"></a>
+## 3. Licence
 The Project you are currently viewing is licenced under the MIT licence.
-## Contributers
+<a name="Con"></a>
+## 4. Contributing
 This Project was contributed by the following Person/People: 
-## Tests
+<a name="Ts"></a>
+## 5. Tests
 Testing of this Program can simply br done by: .
-<a name="intro"></a>
-## Questions
+<a name="Que"></a>
+## 6. Questions
 To view my Github profile visit the link: https://github.com/
 or contact me by email at 
--- index.js
+++ index.js
@@ -8,26 +8,30 @@ const generateReadme = ({ Title, Description, Installation, Usage, Licence, Lice
   ## Description
   ${Description}
   ## Table OF Contents
-  * [Installation](#Intro)
-  * [Usage](##Usage)
-  * [Licence](##Licence)
-  * [Contributers](##Contributers)
-  * [Tests](##Tests)
-  * [Questions](##Questions)
+  1. [ Installation. ](#install)
+  2. [ Usage. ](#Usg)
+  3. [ Licence. ](#Lic)
+  4. [ Contributing. ](#Con)
+  5. [ Tests. ](#Ts)
+  6. [ Questions. ](#Que)
   
-
-## Installation
+<a name="install"></a>
+## 1. Installation
 In order for this Program to run, ${Installation} is required to be installed.
-## Usage
+<a name="#Usg"></a>
+## 2. Usage
 Using this Program requires you to: ${Usage}.
-## Licence
+<a name="Lic"></a>
+## 3. Licence
 The Project you are currently viewing is licenced under the ${Licence} licence.
-## Contributers
+<a name="Con"></a>
+## 4. Contributing
 This Project was contributed by the following Person/People: ${Contributing}
-## Tests
+<a name="Ts"></a>
+## 5. Tests
 Testing of this Program can simply br done by: ${Tests}.
-<a name="intro"></a>
-## Questions
+<a name="Que"></a>
+## 6. Questions
 To view my Github profile visit the link: https://github.com/${GithubUsername}
 or contact me by email at ${EmailAddress}
 `;
